29a92c921ec6c40c12923d881cb1bd49d1dafcdf
Michel Matos <michel.mef@gmail.com>
2023-04-05 23:23:49 -0400

    Finalizado editar

--- src/js/components/product-item.js
+++ src/js/components/product-item.js
@@ -1,25 +1,30 @@
 const product = ({produto, quantidade, valor, id = null}) => {
   const valorConvertido = Number(valor.replace(/,/g, ".")).toLocaleString('pt-br', {style: 'currency', currency:'BRL'})
-  const produtId = id || crypto.randomUUID();
+  const productId = id || crypto.randomUUID();
 
-  const item = `
-    <div data-product="${produtId}" class="section__item">
+  const itemHtml = `
+    <div data-product="${productId}" class="section__item">
       <span class="section__item-1" data-nome="${produto}">${produto}</span>
       <div class="section__item-info">
         <span class="section__item-3" data-quantidade="${quantidade}">Quantidade: ${quantidade}.00UN</span>
         <span class="section__item-4" data-valor="${valor}">Valor Unitario: ${valorConvertido}</span>
       </div>
       <div class="section__item-action">
-        <i class="fa-regular fa-pen-to-square" data-editar="${produtId}"></i>
+        <i class="fa-regular fa-pen-to-square" data-editar="${productId}"></i>
         <i class="fa-regular fa-trash-can"></i>
       </div>
     </div>`
 
-    const calculoPorItem = () => Number(quantidade * valor);
+    const calculoPorItem = () => Number(quantidade * valor).toFixed(2);
+
+    const obj = {
+      id:productId,
+      calculo:calculoPorItem()
+    }
 
     return {
-      item,
-      calculoPorItem
+      item: itemHtml,
+      obj
     }
 }
 
--- src/js/components/products.js
+++ src/js/components/products.js
@@ -2,56 +2,71 @@ import product from "./product-item.js";
 import ScrollLista from "./renderScroll.js";
 import Utils from "../utils/tratar-dados.js";
 
-export default function Products(renderModal){
+export default function Products(renderModal) {
   const produtos = document.querySelector('[data-products="products"]')
   const total = document.querySelector('[data-total]');
   const itemsCalculos = []
 
   const addProduto = items => {
-      
+
     save(items, renderizarNovoProduto);
   }
 
-  function save(items, fn){
-    const {item, calculoPorItem } = product(items)
-    
-    fn(item, calculoPorItem);
+  function save(items, fn) {
+    const { item, obj } = product(items)
+
+    fn(item, obj);
     exibirMoeda(itemsCalculos);
     ScrollLista(produtos)
   }
 
-  const editarProduto = items => {
-    save(items)
+  const editarObjetoNoArray = (obj) => {
+    itemsCalculos
+      .filter(({ id, calculo }) => id == obj.id && calculo != obj.calculo)
+      .map(({ id }) => itemsCalculos.forEach(ic => {
+        if (ic['id'] == id) ic['calculo'] = obj['calculo']
+      }));
   }
-  
-  const exibirMoeda = (itemsCalculos) => {   
-   
+
+  const editarProduto = items => {
+    save(items, (item, obj) => {
+      
+      const element = Utils.convertTextoEmHtml(item, `[data-product="${obj.id}"]`)
+      editarObjetoNoArray(obj)
+      const elementoOld = produtos.querySelector(`[data-product="${obj.id}"]`)
+      
+      produtos.replaceChild(element, elementoOld)
+    })
+  }
+
+  const exibirMoeda = (itemsCalculos) => {
+
     const valor = calculo(itemsCalculos)
     total.innerHTML = ''
     total.innerHTML = converterEmMoeda(valor)
   }
 
-  function renderizarNovoProduto(item, calculoPorItem) {
-    
-    itemsCalculos.push(calculoPorItem());
-    produtos.insertAdjacentHTML('beforeend', item);  
+  function renderizarNovoProduto(item, obj) {
+
+    itemsCalculos.push(obj);
+    produtos.insertAdjacentHTML('beforeend', item);
   }
 
   const exibirDadosParaEditar = elementoClicado => {
-    
-      const idEditar = elementoClicado.dataset.editar
-      const valores = Utils.tratarDadosItemsProduto(produtos, idEditar)
-      valores['id'] = idEditar
-      renderModal(addProduto, valores)
+
+    const idEditar = elementoClicado.dataset.editar
+    const valores = Utils.tratarDadosItemsProduto(produtos, idEditar)
+    valores['id'] = idEditar
+    renderModal(editarProduto, valores)
   }
 
   produtos.addEventListener('click', e => {
     const elementoClicado = e.target
-    if(elementoClicado.dataset.editar){
+    if (elementoClicado.dataset.editar) {
       exibirDadosParaEditar(elementoClicado)
     }
   })
-  
+
   return {
     produtos,
     addProduto
@@ -60,12 +75,12 @@ export default function Products(renderModal){
 
 
 const calculo = valores => {
-  const soma = (acc, item) => acc += Number(item);
-  return valores.reduce(soma ,0)
+  const soma = (acc, { calculo }) => acc += Number(calculo);
+  return valores.reduce(soma, 0)
 }
 
-const converterEmMoeda = valor => 
-  valor.toLocaleString('pt-br', {style: 'currency', currency:'BRL'})
+const converterEmMoeda = valor =>
+  valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 
 
 
